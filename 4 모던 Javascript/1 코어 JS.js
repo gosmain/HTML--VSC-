5687bcac965d55fe5caafa4797930569d41c84be
gosmain <gosmain@naver.com>
2021-09-14 22:21:47 +0900

    모던 js tut 1

---
+++ 4 모던 Javascript/1 코어 JS.js
@@ -0,0 +1,7 @@
+let age = prompt('나이를 입력해주세요.', 100);
+alert(`당신의 나이는 ${age}살 입니다.`); // 당신의 나이는 100살입니다.
+
+
+let isBoss = confirm("당신이 주인인가요?"); // result = confirm(question);
+alert( isBoss ); // 확인 버튼을 눌렀다면 true가 출력됩니다.
+
